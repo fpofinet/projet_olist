9136777c50520d6135cdc34dd4d8bcac9d99a8d4
ragellan-dev <lionel@placholder.com>
2026-07-14 23:12:36 +0000

    finalisation de l'analys exploratoire des orders

---
+++ analyses/analyse_order.sql
@@ -0,0 +1,61 @@
+-- execution des requetes avec duckdb .read './analyses/analyse_order.sql'
+
+-- -- on calcule le nombre total de commande
+-- SELECT count(*)
+-- FROM read_csv_auto('data/olist_orders_dataset.csv');
+
+--- on fait une description de notre dataset 
+-- DESCRIBE 'data/olist_orders_dataset.csv';
+-- -- On compte les commandes qui ont une date de livraison effective superieur a la date estime 
+-- SELECT COUNT(*)
+-- FROM read_csv_auto('data/olist_orders_dataset.csv')
+-- WHERE order_estimated_delivery_date < order_delivered_customer_date
+
+-- -- on affiche une petite partie des donnees 
+-- SELECT *
+-- FROM  read_csv_auto('data/olist_orders_dataset.csv')
+-- LIMIT 20;
+-- SUMMARIZE 'data/olist_orders_dataset.csv';
+
+-- SELECT count(*)
+-- FROM read_csv_auto('data/olist_order_items_dataset.csv');
+
+--- on fait une description du dataset des produits de la commande
+-- DESCRIBE 'data/olist_order_items_dataset.csv';
+-- -- on affiche une petite partie des donnees
+-- SELECT *
+-- FROM  read_csv_auto('data/olist_order_items_dataset.csv')
+-- LIMIT 20;
+-- SUMMARIZE 'data/olist_order_items_dataset.csv';
+
+-- SELECT count(*)
+-- FROM read_csv_auto('data/olist_order_reviews_dataset.csv');
+-- --- on fait une description du dataset order reviews
+-- DESCRIBE 'data/olist_order_reviews_dataset.csv';
+-- -- on affiche une petite partie des donnees
+-- SELECT *
+-- FROM  read_csv_auto('data/olist_order_reviews_dataset.csv')
+-- LIMIT 20;
+-- --- On verifie s'il y'a des reviews qui ont des dates incoherentes
+-- SELECT COUNT(*) AS total_aberrante
+-- FROM  read_csv_auto('data/olist_order_reviews_dataset.csv')
+-- WHERE review_creation_date > review_answer_timestamp;
+
+-- SUMMARIZE 'data/olist_order_reviews_dataset.csv';
+
+-- SELECT count(*)
+-- FROM read_csv_auto('data/olist_order_payments_dataset.csv');
+-- --- on fait une description du dataset
+DESCRIBE 'data/olist_order_payments_dataset.csv';
+-- -- on affiche une petite partie des donnees 
+-- SELECT *
+-- FROM  read_csv_auto('data/olist_order_payments_dataset.csv')
+-- LIMIT 20;
+
+-- --- on affiche quelques statistique de notre dataset
+SUMMARIZE 'data/olist_order_payments_dataset.csv';
+SELECT COUNT(*), payment_type 
+FROM read_csv_auto('data/olist_order_payments_dataset.csv')
+GROUP BY payment_type;
+
+
